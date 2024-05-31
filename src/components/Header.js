ad28b8e7252b00290edbf2033cdbca49a96da71f
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-31 12:57:24 +0200

    finished all styling for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import ThemeSelector from "./theme";
 export default function Header() {
   return (
     <header className="header">
-      <div >
+  
         <img className="logoImg" src="https://t4.ftcdn.net/jpg/01/13/99/57/360_F_113995750_dAEGvjqxnsYD6asKjeDWJoVoSqjFvdGO.jpg" />
-      </div>
+     
       <div className="link-buttons">
         <button className="linkButton">About Me</button>
         <button className="linkButton">My Best Projects</button>
