0fa3d51ed74a96a2e71e5180088f51a6772b9e3a
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-06-04 10:50:09 +0200

    added some styling

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,79 +1,51 @@
 import React from "react";
-import workPhoto from "./images/445762120_8092350080775485_857915379553477487_n.jpg"
-import taskManager from './images/agileBoard.png'
-import bookApp from './images/bookApp.png'
+import workPhoto from "./images/445762120_8092350080775485_857915379553477487_n.jpg";
+import taskManager from "./images/agileBoard.png";
+import bookApp from "./images/bookApp.png";
 
-export default function MyWork(){
-
-    return(
-
-        <div className="MyWork">
-            <h1>MY WORK</h1>
-            <div className="cardContainer">
-
-            <div className="card" >
-                <div>
-                    <img className="cardImg" src={taskManager} />
-                </div>
-                <div className="cardText">
-                    <h4 >Task manager app</h4>
-                    <p>Description djfnvldjvndfvkdjv dfn j hi jndjk ljbn jkb kj ljn nk jn ljn jkl nj nlk nlkj This is an application buit with html, javascript and css. it is a task manager app that is highly interactive.</p>
-                
-                </div>
-                <a href="https://task-manager-appli.netlify.app" target="_blank">
-                Visit my site here
-                </a>
-            </div>
-            <div className="card">
-                <div>
-                    <img className="cardImg" src={bookApp}  />
-                </div>
-                <div className="cardText">
-                    <h4 >Book App</h4>
-                    <p>Description djfnvldjvndfvkdjv dfn j hi jndjk ljbn jkb kj ljn nk jn ljn jkl nj nlk nlkj </p>
-                </div>
-                <a href="https://bookapp-buh.netlify.app/" target="_blank">
-                   
-                        Visit my site here
-                    
-                </a>
-
-            </div>
-            <div className="card">
-                <div>
-                    <img className="cardImg" src={workPhoto} />
-                </div>
-                <div className="cardText">
-                    <h4 >Name of project</h4>
-                    <p>Description djfnvldjvndfvkdjv dfn j hi jndjk ljbn jkb kj ljn nk jn ljn jkl nj nlk nlkj </p>
-                </div>
-              
-
-            </div>
-            <div className="card">
-                <div>
-                    <img className="cardImg" src={workPhoto} />
-                </div>
-                <div className="cardText">
-                    <h4 >Name of project</h4>
-                    <p>Description djfnvldjvndfvkdjv dfn j hi jndjk ljbn jkb kj ljn nk jn ljn jkl nj nlk nlkj </p>
-                </div>
-               
-            </div>
-            <div className="card">
-                <div>
-                    <img className="cardImg" src={workPhoto} />
-                </div>
-                <div className="cardText">
-                    <h4 >Name of project</h4>
-                    <p>Description djfnvldjvndfvkdjv dfn j hi jndjk ljbn jkb kj ljn nk jn ljn jkl nj nlk nlkj </p>
-                </div>
-           
-
-            </div>
-
+export default function MyWork() {
+  return (
+    <div className="MyWork">
+      <h1>MY WORK</h1>
+      <div className="cardContainer">
+        <div className="card">
+          <div>
+            <img className="cardImg" src={taskManager} />
+          </div>
+          <div className="cardText">
+            <h4>Task manager app</h4>
+            <p>
+              Discover our highly interactive Task Manager App, built with HTML,
+              JavaScript, and CSS. It features a sleek design and intuitive
+              interface for managing your tasks efficiently. Enjoy customizable
+              task lists, drag-and-drop functionality, and theme persistence via
+              local storage, ensuring a personalized and seamless experience
+              every time.
+            </p>
+          </div>
+          <a href="https://task-manager-appli.netlify.app" target="_blank">
+            Visit my site here
+          </a>
         </div>
-        
+        <div className="card">
+          <div>
+            <img className="cardImg" src={bookApp} />
+          </div>
+          <div className="cardText">
+            <h4>Book App</h4>
+            <p>
+              Explore our innovative Book App, designed with HTML, JavaScript,
+              and CSS. Easily filter books by name, genre, and latest updates,
+              or search for specific titles. The app offers a smooth and
+              engaging user experience, with your preferences saved in local
+              storage for a consistent and tailored reading journey.
+            </p>
+          </div>
+          <a href="https://bookapp-buh.netlify.app/" target="_blank">
+            Visit my site here
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
